0623f55fc3c3cd14be5e591faedd601a22549e2f
Otwgoat <lucas.jouffroy@gmail.com>
2023-07-19 23:34:16 +0200

    Preparation du deploiement

--- migrations/Version20230623093153.php
+++ migrations/Version20230623093153.php
@@ -20,7 +20,7 @@ final class Version20230623093153 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE rate_review DROP FOREIGN KEY fk_rate_review_user_id');
+        //$this->addSql('ALTER TABLE rate_review DROP FOREIGN KEY fk_rate_review_user_id');
         $this->addSql('ALTER TABLE rate_review ADD relation VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE rate_review ADD CONSTRAINT FK_DBB4B0ADA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
     }
