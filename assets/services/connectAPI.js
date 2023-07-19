39c9e9b2b60f9f3001624ccd2f379d1a371e8051
Otwgoat <lucas.jouffroy@gmail.com>
2023-07-19 22:50:11 +0200

    Preaparation du déploiement

--- assets/services/connectAPI.js
+++ assets/services/connectAPI.js
@@ -2,6 +2,8 @@ function apiPath(path) {
     
   if (process.env.NODE_ENV ===  'development'){
    return 'http://localhost:8000/api/' + path;
+    } else {
+      return 'https://sandrine-coupart-webapp.herokuapp.com/api/' + path;
     }
 }
 
