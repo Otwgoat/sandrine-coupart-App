a0b23e3d0e28d23840b6a261493e613a0c420deb
Otwgoat <lucas.jouffroy@gmail.com>
2023-07-20 11:16:05 +0200

    Enable CORS

--- assets/services/connectAPI.js
+++ assets/services/connectAPI.js
@@ -3,7 +3,7 @@ function apiPath(path) {
   if (process.env.NODE_ENV ===  'development'){
    return 'http://localhost:8000/api/' + path;
     } else {
-      return 'https://sandrine-coupart-webapp.herokuapp.com/api/' + path;
+      return 'https://sandrine-coupart-webapp-74b3dd582601.herokuapp.com/api/' + path;
     }
 }
 
